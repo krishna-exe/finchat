b4dbc97cc7cf837ed1c3ed5116359fb11ac05b0a
Krishna Rudani <krishnarudani16@gmail.com>
2024-03-13 23:40:48 +0530

    added authentication using Clerk

---
+++ src/app/sign-up/[[...sign-up]]/page.tsx
@@ -0,0 +1,7 @@
+import { SignUp } from "@clerk/nextjs";
+
+export default function Page(){
+    <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+      <SignUp />
+    </div>
+}
