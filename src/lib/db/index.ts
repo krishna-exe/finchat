ebcc2278c516e12670aa78dd6a4324a63d2a590e
Krishna Rudani <krishnarudani16@gmail.com>
2024-03-17 13:14:22 +0530

    index.ts updated

--- src/lib/db/index.ts
+++ src/lib/db/index.ts
@@ -7,6 +7,6 @@ if (!process.env.DATABASE_URL) {
   throw new Error("database url not found");
 }
 
-const sql = neon(process.env.DATABASE_URL);
+const sql = neon<boolean,boolean>(process.env.DATABASE_URL);
 
 export const db = drizzle(sql);
