cc181fb167a3332feeaa06957f49fe437c8bddfd
Krishna Rudani <krishnarudani16@gmail.com>
2024-03-21 13:32:22 +0530

    css changes

--- src/components/ChatSideBar.tsx
+++ src/components/ChatSideBar.tsx
@@ -16,14 +16,15 @@ const ChatSideBar = ({ chats, chatId }: Props) => {
   const [loading, setLoading] = React.useState(false);
 
   return (
-    <div className="w-full max-h-screen overflow-scroll soff p-4 text-gray-200 bg-gray-900">
+    <div className="w-full h-screen p-4 text-gray-200 bg-gray-900">
       <Link href="/">
         <Button className="w-full border-dashed border-white border">
           <PlusCircle className="mr-2 w-4 h-4" />
           New Chat
         </Button>
       </Link>
-      <div className="flex max-h-screen overflow-scroll pb-20 flex-col gap-2 mt-4">
+      {/* <div className="flex max-h-screen overflow-scroll pb-20 flex-col gap-2 mt-4"> */}
+      <div className="flex flex-col gap-2 mt-4">
         {chats.map((chat) => (
           <Link key={chat.id} href={`/chat/${chat.id}`}>
             <div
