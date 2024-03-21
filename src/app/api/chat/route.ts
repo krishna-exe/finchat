dbf49c72876533cf5b8d19e72e6ac10811e4352c
Krishna Rudani <krishnarudani16@gmail.com>
2024-03-21 19:44:21 +0530

    added context fetching and changed response stream

--- src/app/api/chat/route.ts
+++ src/app/api/chat/route.ts
@@ -1,5 +1,10 @@
 import {GoogleGenerativeAI,HarmCategory,HarmBlockThreshold,} from "@google/generative-ai";
 import { GoogleGenerativeAIStream,StreamingTextResponse,Message } from "ai";
+import { db } from "@/lib/db";
+import { chats, messages as _messages } from "@/lib/db/schema";
+import { eq } from "drizzle-orm";
+import { NextResponse } from "next/server";
+import { getContext } from "@/lib/context";
 
 export const runtime="edge";
 const genai= new GoogleGenerativeAI(process.env.GEMINI_API_KEY!)
@@ -13,24 +18,100 @@ const genai= new GoogleGenerativeAI(process.env.GEMINI_API_KEY!)
 //       })),
 //   });
 
+// export async function POST(req:Request){
+//     //TODO: Complete this after understanding message type
+//     try {
+//         const {messages}=await req.json();
+//         console.log(typeof messages)
+//         const response=await genai.getGenerativeModel({model:"gemini-pro"})
+//             .generateContentStream(...messages.filter())
+//         //         .generateContentStream({
+//         //     contents:[{role:'user',parts:[{text:messages[0].content}]}]
+//         // });
+//         const stream=GoogleGenerativeAIStream(response)
+//         return new StreamingTextResponse(stream)
+//     } catch (error) {
+//         console.error(error)
+//     }
+// }
+
 export async function POST(req:Request){
     //TODO: Complete this after understanding message type
     try {
-        const {messages}=await req.json();
-        console.log(typeof messages)
-        const response=await genai.getGenerativeModel({model:"gemini-pro"})
-            .generateContentStream(...messages.filter())
-        //         .generateContentStream({
-        //     contents:[{role:'user',parts:[{text:messages[0].content}]}]
-        // });
-        const stream=GoogleGenerativeAIStream(response)
+        const {messages, chatId}=await req.json();
+        const _chats = await db.select().from(chats).where(eq(chats.id, chatId));
+        if (_chats.length != 1) {
+            return NextResponse.json({ error: "chat not found" }, { status: 404 });
+          }
+        const fileKey = _chats[0].fileKey;
+        const lastMessage = messages[messages.length - 1];
+        const context = await getContext(lastMessage.content, fileKey);
+        console.log(context)
+
+        // console.log(messages[messages.length - 1].content)
+        
+        const prompt={
+            role: "system",
+            content: `AI assistant is a brand new, powerful, human-like artificial intelligence.
+            The traits of AI include expert knowledge, helpfulness, cleverness, and articulateness.
+            AI is a well-behaved and well-mannered individual.
+            AI is always friendly, kind, and inspiring, and he is eager to provide vivid and thoughtful responses to the user.
+            AI has the sum of all knowledge in their brain, and is able to accurately answer nearly any question about any topic in conversation.
+            AI assistant is a big fan of Pinecone and Vercel.
+            START CONTEXT BLOCK
+            ${context}
+            END OF CONTEXT BLOCK
+            AI assistant will take into account any CONTEXT BLOCK that is provided in a conversation.
+            If the context does not provide the answer to question, the AI assistant will say, "I'm sorry, but I don't know the answer to that question".
+            AI assistant will not apologize for previous responses, but instead will indicated new information was gained.
+            AI assistant will not invent anything that is not drawn directly from the context.
+            `,
+        }
+        
+        const response=await genai
+            .getGenerativeModel({model:"gemini-pro"})
+            .generateContentStream(lastMessage.content);
+            const stream=GoogleGenerativeAIStream(response, {
+                onStart: async () => {
+                    await db.insert(_messages).values({
+                        chatId,
+                        content: lastMessage.content,
+                        role:'user',
+                    });
+                },
+                onCompletion: async (completion) =>{
+                    await db.insert(_messages).values({
+                        chatId,
+                        content:completion,
+                        role:'system',
+                    });
+                },
+            });
+            // console.log(lastMessage)
         return new StreamingTextResponse(stream)
     } catch (error) {
         console.error(error)
     }
 }
 
-
+// const stream = GoogleGenerativeAIStream(response, {
+//     onStart: async () => {
+//       // save user message into db
+//       await db.insert(_messages).values({
+//         chatId,
+//         content: lastMessage.content,
+//         role: "user",
+//       });
+//     },
+//     onCompletion: async (completion) => {
+//       // save ai message into db
+//       await db.insert(_messages).values({
+//         chatId,
+//         content: completion,
+//         role: "system",
+//       });
+//     },
+//   });
 
 
         // const generationConfig = {
---
+++ src/lib/context.ts
@@ -0,0 +1,46 @@
+import { Pinecone } from "@pinecone-database/pinecone";
+import { convertToAscii } from "./utils";
+import { getEmbeddings } from "./embeddings";
+
+export async function getMatchesFromEmbeddings(
+  embeddings: number[],
+  fileKey: string
+) {
+  try {
+    const client = new Pinecone({
+        apiKey: process.env.PINECONE_API_KEY!,
+      });
+    const index = client.index('reports');
+    const namespace = index.namespace(convertToAscii(fileKey));
+    const queryResult = await namespace.query({
+      topK: 5,
+      vector: embeddings,
+      includeMetadata: true,
+    });
+    return queryResult.matches || [];
+  } catch (error) {
+    console.log("error querying embeddings", error);
+    throw error;
+  }
+}
+
+export async function getContext(query: string, fileKey: string) {
+    
+    const queryEmbeddings = await getEmbeddings(query);
+    const matches = await getMatchesFromEmbeddings(queryEmbeddings, fileKey);
+    
+    // console.log("------------------------------------------------\nThese are the qualifying docs\n--------------------------\n"+matches+" ")
+    const qualifyingDocs = matches.filter(
+        (match) => match.score && match.score > 0.7
+        );
+        
+        type Metadata = {
+            text: string;
+            pageNumber: number;
+        };
+
+  let docs = qualifyingDocs.map((match) => (match.metadata as Metadata).text);
+  // 5 vectors
+  console.log(docs)
+  return docs.join("\n").substring(0, 3000);
+}
