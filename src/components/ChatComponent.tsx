1a4707be11daefcdd1eb22c28080379275ca347b
Krishna Rudani <krishnarudani16@gmail.com>
2024-04-04 19:17:50 +0530

    edit css

--- src/components/ChatComponent.tsx
+++ src/components/ChatComponent.tsx
@@ -3,7 +3,7 @@ import React from "react";
 import { Input } from "./ui/input";
 import { useChat } from "ai/react";
 import { Button } from "./ui/button";
-import { Send } from "lucide-react";
+import { Send, Loader2, CircleStop  } from "lucide-react";
 import MessageList from "./MessageList";
 import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
@@ -13,7 +13,7 @@ import Markdown from 'react-markdown'
 type Props = { chatId: number };
 
 const ChatComponent = ({ chatId }: Props) => {
-  const { data, isLoading } = useQuery({
+  const { data } = useQuery({
     queryKey: ["chat", chatId],
     queryFn: async () => {
       const response = await axios.post<Message[]>("/api/get-messages", {
@@ -23,7 +23,7 @@ const ChatComponent = ({ chatId }: Props) => {
     },
   });
 
-  const { input, handleInputChange, handleSubmit, messages } = useChat({
+  const { input, handleInputChange, handleSubmit, messages, isLoading, stop } = useChat({
     api: "/api/chat",
     body: {
       chatId,
@@ -58,13 +58,19 @@ const ChatComponent = ({ chatId }: Props) => {
       >
         <div className="flex">
           <Input
+            type="text"
             value={input}
+            disabled={isLoading}
             onChange={handleInputChange}
-            placeholder="Ask any question..."
+            placeholder={isLoading ?"Generating . . ." :"Ask any question..."}
             className="w-full"
           />
-          <Button className="bg-blue-600 ml-2">
-            <Send className="h-4 w-4" />
+          <Button className="bg-blue-600 ml-2 rounded-full">
+			{isLoading ? 
+				(<Loader2 onClick={stop} className="h-4 w-4 animate-spin"/>) 
+				: 
+				(<Send className="h-4 w-4" />)}
+            
           </Button>
         </div>
       </form>
--- src/components/MessageList.tsx
+++ src/components/MessageList.tsx
@@ -1,8 +1,9 @@
 import { cn } from "@/lib/utils";
 import { Message } from "ai/react";
-import { Loader2 } from "lucide-react";
+import { Loader2, User, BotMessageSquare   } from "lucide-react";
 import React from "react";
 import ReactMarkdown from 'react-markdown';
+import markdownit from 'markdown-it';
 
 type Props = {
   isLoading: boolean;
@@ -10,21 +11,21 @@ type Props = {
 };
 
 const MessageList = ({ messages, isLoading }: Props) => {
-  if (isLoading) {
-    return (
-      <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
-        <Loader2 className="w-6 h-6 animate-spin" />
-      </div>
-    );
-  }
+  // if (isLoading) {
+  //   return (
+  //     <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+  //       <Loader2 className="w-6 h-6 animate-spin" />
+  //     </div>
+  //   );
+  // }
   if (!messages) return <></>;
   return (
     <div className="flex flex-col gap-2 px-4">
-      {messages.map((message) => {
-        const Icon = message.role === "user"? User : Bot;
-        const icon = (
-          <Icon size={16} className={message.role === "user"? "absolute " : "absolute"} />
-        );
+      {messages.map((message, index) => {
+        // const Icon = message.role === "user"? User : Bot;
+        // const icon = (
+          // <Icon size={16} className={message.role === "user"? "absolute " : "absolute"} />
+        // );
         return (
           <div
             key={message.id}
@@ -33,15 +34,26 @@ const MessageList = ({ messages, isLoading }: Props) => {
               "justify-start pr-10": message.role === "assistant",
             })}
           >
+           
             <div
               className={cn(
-                "rounded-lg px-3 text-sm py-1 shadow-md ring-1 ring-gray-900/10",
+                "rounded-lg px-3 text-m py-2 shadow-md ring-1 ring-gray-900/10 ml-6 relative mb-3",
                 {
-                  "bg-blue-600 text-white": message.role === "user",
+                  "bg-blue-600 shadow-md ring-1 text-white mr-6 ": message.role === "user",
                 }
               )}
             >
               <p><ReactMarkdown>{message.content}</ReactMarkdown></p>
+               {/* {icon} */}
+              {message.role === 'user' ? 
+                <User size={30} color='black'   
+                      className="absolute top-1 -right-8 rounded-full shadow-lg ring-gray-900/10 ml-0 p-1"
+                      style={{ marginLeft: '4px' }}/> : 
+                <BotMessageSquare  size={30} color='black'  
+                      className={`absolute top-1 -left-9 rounded-full shadow-lg ml-0 p-1 stroke-[#0842AB]
+                      ${ 
+                        isLoading && index===messages.length-1 ? 'animate-bounce' : ""
+                      }` }/>}
             </div>
           </div>
         );
