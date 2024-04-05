c3b70bdee9e1a58e4171de684a07e329efae6077
Krishna Rudani <krishnarudani16@gmail.com>
2024-04-05 16:03:51 +0530

    edit markdown and add greeting message

--- src/components/Markdown.tsx
+++ src/components/Markdown.tsx
@@ -1,7 +1,19 @@
-// import React from 'react'
+import React from 'react';
+import markdownit from 'markdown-it';
+import DOMPurify from 'dompurify';
 
-// type Props= {
+type Props= {
+    text: string;
+}
 
-// }
+const md = markdownit({
 
-// cons
+});
+
+const Markdown = ({text}: Props) => {
+    const htmlcontent = md.render(text);
+    const sanitized = DOMPurify.sanitize(htmlcontent);
+    return <div dangerouslySetInnerHTML={{ __html:sanitized}}></div>;
+}
+
+export  default Markdown;
--- src/components/MessageList.tsx
+++ src/components/MessageList.tsx
@@ -3,7 +3,9 @@ import { Message } from "ai/react";
 import { Loader2, User, BotMessageSquare   } from "lucide-react";
 import React from "react";
 import ReactMarkdown from 'react-markdown';
-// import markdownit from 'markdown-it';
+import markdownit from 'markdown-it';
+// import Markdown from "react-markdown";
+import Markdown from  '@/components/Markdown'
 
 type Props = {
   isLoading: boolean;
@@ -18,10 +20,17 @@ const MessageList = ({ messages, isLoading }: Props) => {
   //     </div>
   //   );
   // }
-  if (!messages) return <></>;
+  const greetingMessage = {
+    id: 'greeting',
+    role: 'assistant',
+    content: `Hello! How can I assist you today?\nThese are the questions that can be asked related to the given PDF:\n`,
+  }
+
+  const allmessages = [greetingMessage,...messages];
+  if (!allmessages) return <></>;
   return (
     <div className="flex flex-col gap-2 px-4">
-      {messages.map((message, index) => {
+      {allmessages.map((message, index) => {
         // const Icon = message.role === "user"? User : Bot;
         // const icon = (
           // <Icon size={16} className={message.role === "user"? "absolute " : "absolute"} />
@@ -37,13 +46,14 @@ const MessageList = ({ messages, isLoading }: Props) => {
            
             <div
               className={cn(
-                "rounded-lg px-3 text-m py-2 shadow-md ring-1 ring-gray-900/10 ml-6 relative mb-3",
+                "mt-1 rounded-lg px-3 text-m py-2 shadow-md ring-1 ring-gray-900/10 ml-6 relative mb-3",
                 {
                   "bg-blue-600 shadow-md ring-1 text-white mr-6 ": message.role === "user",
                 }
               )}
             >
-              <p><ReactMarkdown>{message.content}</ReactMarkdown></p>
+              {/* <p><ReactMarkdown>{message.content}</ReactMarkdown></p> */}
+              <p><Markdown text={message.content}/></p>
                {/* {icon} */}
               {message.role === 'user' ? 
                 <User size={30} color='black'   
